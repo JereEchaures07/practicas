aac6b0ba7502dd711314cd9399104fe41efb0eed
JereEchaures07 <jerechaures@gmail.com>
2025-04-29 10:43:11 -0300

    modificando cards

--- proyecto-mio/src/app/paginas/carrito/carrito.component.ts
+++ proyecto-mio/src/app/paginas/carrito/carrito.component.ts
@@ -1,15 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { CarritoService } from '../../servicio/carrito.service';
+import { Producto } from '../../model/producto.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-carrito',
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './carrito.component.html',
   styleUrl: './carrito.component.css'
 })
 export class CarritoComponent implements OnInit {
-productosEncarrito: {producto: Producto; cantidad: number}[] = []
-constructor(private carritoService: carritoS)
+  productosEncarrito: { producto: Producto; cantidad: number }[] = []
 
+  constructor(private carritoService: CarritoService) { }
+
+  ngOnInit(): void {
+    this.carritoService.carrito$.subscribe((productos) => {
+      this.productosEncarrito = productos
+    })
+  }
+  agregarCantidad(index: number) {
+    this.productosEncarrito[index].cantidad++
+  }
+
+  quitarCantidad(index: number) {
+    if (this.productosEncarrito[index].cantidad > 1) {
+      this.productosEncarrito[index].cantidad--
+    }
+  }
+  
+  eliminarProducto(productoId:number){
+    this.carritoService.eliminarDelCarrito(productoId)
+  }
+
+  vaciarCarrito(){
+    this.carritoService.VaciarCarrito()
+  }
+ 
+  realizarCompra(){
+    alert('compra Realizada')
+    this.vaciarCarrito()
+  }
 
 }
+
--- proyecto-mio/src/app/paginas/productos/productos.component.ts
+++ proyecto-mio/src/app/paginas/productos/productos.component.ts
@@ -1,10 +1,12 @@
-import { NgFor } from '@angular/common';
+import { CommonModule, NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
+import { Producto } from '../../model/producto.model';
+import { CarritoService } from '../../servicio/carrito.service';
 
 @Component({
   selector: 'app-producto',
-  imports: [FormsModule, NgFor],
+  imports: [FormsModule, NgFor, CommonModule],
   templateUrl: './productos.component.html',
   styleUrl: './productos.component.css'
 })
@@ -21,11 +23,41 @@ export class ProductosComponent {
   nombre :'jere',
   activo: true
   };
-  */
+  
 
   productos = [
    { nombre: 'iphone 16', precio: 1200, img: 'https://www.1p.sg/cdn/shop/files/iPhone-16-Black_1cebd178-ed7b-4386-983e-e3aca356bd31.jpg?v=1728668203&width=1445', desc: '' },
    { nombre: 'iphone 15 pro max', precio: 1500, img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh5bhIS8u-u8BeZPMDi0NnxgzLacyrDteu7A&s', desc: '' },
   ]
+  */
+
+  Productos: Producto[] = [
+    {
+      id: 1,
+      nombre: 'iphone 16',
+      descripcion: '',
+      precio: 24000,
+      imagen: 'https://www.1p.sg/cdn/shop/files/iPhone-16-Black_1cebd178-ed7b-4386-983e-e3aca356bd31.jpg?v=1728668203&width=1445',
+      disponible: true,
+    },
+    {
+      id: 2,
+      nombre: 'iphone 15 pro max',
+      descripcion: '',
+      precio: 20000,
+      imagen: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRh5bhIS8u-u8BeZPMDi0NnxgzLacyrDteu7A&s',
+      disponible: true,
+    },
+
+  ]
+  constructor(private carritoService: CarritoService) { }
+  // Metodo para agregar un producto
+
+  agregar(producto: Producto) {
+    this.carritoService.agregarAlcarrito(producto)
+    alert('producto agregado al carrito')
+  }
 }
+
+
 
